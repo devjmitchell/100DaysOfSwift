f68b46ad5a5d20a6bd2af6a51a3c90331e99ead3
Jason Mitchell <devjmitchell@gmail.com>
2019-03-10 17:59:45 -0700

    Complete initial project following tutorial

---
+++ Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Jason Mitchell on 3/10/19.
+//  Copyright © 2019 Jason Mitchell. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
