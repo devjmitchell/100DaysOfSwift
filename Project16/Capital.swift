bbe71009677db87adb30c589491efbf62d91cb99
Jason Mitchell <devjmitchell@gmail.com>
2019-04-13 16:34:45 -0700

    Complete project following tutorial

---
+++ Project16/Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by Jason Mitchell on 4/13/19.
+//  Copyright © 2019 Jason Mitchell. All rights reserved.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
