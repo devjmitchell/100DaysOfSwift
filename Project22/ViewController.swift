c839bfd8f2d070b07c2f3533b9b4d1d97a086802
Jason Mitchell <devjmitchell@gmail.com>
2019-05-01 23:15:33 -0700

    Track multiple beacons

--- Project22/ViewController.swift
+++ Project22/ViewController.swift
@@ -12,8 +12,15 @@ import UIKit
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet var distanceReading: UILabel!
+    @IBOutlet var beaconLabel: UILabel!
     var locationManager: CLLocationManager?
-    var beaconAlreadyFound = false
+    
+    let beacons: [UUID: String] = [
+        UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!: "Beacon 1",
+        UUID(uuidString: "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0")!: "Beacon 2",
+    ]
+    
+    var currentBeacon: UUID?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,21 +43,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func startScanning() {
-        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
-        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
-        
-        locationManager?.startMonitoring(for: beaconRegion)
-        locationManager?.startRangingBeacons(in: beaconRegion)
+        for (uuid, identifier) in beacons {
+            let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: identifier)
+            locationManager?.startMonitoring(for: beaconRegion)
+            locationManager?.startRangingBeacons(in: beaconRegion)
+        }
     }
     
     func update(distance: CLProximity) {
-        if distance != .unknown {
-            if !beaconAlreadyFound {
-                beaconAlreadyFound = true
-                presentFoundAlert()
-            }
-        }
-        
         UIView.animate(withDuration: 1) {
             switch distance {
             case .far:
@@ -68,22 +68,28 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             default:
                 self.view.backgroundColor = .gray
                 self.distanceReading.text = "UNKNOWN"
-                self.beaconAlreadyFound = false
+                self.beaconLabel.text = ""
             }
         }
-    }
-    
-    func presentFoundAlert() {
-        let ac = UIAlertController(title: "Beacon Found", message: nil, preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Let's find it!", style: .default))
-        present(ac, animated: true)
     }
     
     func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
         if let beacon = beacons.first {
             update(distance: beacon.proximity)
-        } else {
-            update(distance: .unknown)
+            
+            if currentBeacon != beacon.proximityUUID {
+                currentBeacon = beacon.proximityUUID
+                
+                let beaconName = self.beacons[currentBeacon!]!
+                beaconLabel.text = beaconName
+                showFoundAlert(for: beaconName)
+            }
         }
+    }
+    
+    func showFoundAlert(for beacon: String) {
+        let ac = UIAlertController(title: "\(beacon) Nearby", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Let's find it!", style: .default))
+        present(ac, animated: true)
     }
 }
