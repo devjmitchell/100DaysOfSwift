9b15d7878ad4411e5794c7bcff88179af62e7419
Jason Mitchell <devjmitchell@gmail.com>
2019-11-07 18:19:22 -0700

    Copy over project 6a files into folder

---
+++ Project6a_GuessTheFlag_withAutoLayout/Project2/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Jason Mitchell on 3/5/19.
+//  Copyright © 2019 Jason Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var totalAnswered = 0
+    var correctAnswer = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Score", style: .plain, target: self, action: #selector(showScore))
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        let currentFlag = countries[correctAnswer].uppercased()
+        title = "\(currentFlag) -- Score: \(score)"
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong... that's the flag of \(countries[sender.tag].uppercased())"
+            score -= 1
+        }
+        
+        totalAnswered += 1
+        
+        if totalAnswered < 10 {
+            let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Game Over!", message: "Final score = \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Play Again!", style: .default, handler: askQuestion))
+            score = 0
+            totalAnswered = 0
+            present(ac, animated: true)
+        }
+    }
+    
+    @objc func showScore() {
+        let ac = UIAlertController(title: "Score", message: "Your current score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(ac, animated: true)
+    }
+}
+
